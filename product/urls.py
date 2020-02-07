cfa0ca440a70da6928d18d6e403918893a6d7534
techehub <vijeesh.tp@techehub.com>
2020-02-07 11:27:23 +0530

    Add to cart

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import myProducts, productlist
+from .views import myProducts, productlist, addItem
 
 urlpatterns = [
 
-    path('product/<str:aaa>',  myProducts ),
+    path('product/<str:pid>',  myProducts ),
     path('items', productlist),
+    path ('addItem', addItem)
 
 
 ]
--- product/views.py
+++ product/views.py
@@ -7,27 +7,61 @@ from django.template import loader
 from .models import Product
 
 def productlist (req):
+
+    #p_id = req.COOKIES ['productid']
+    p_id = req.COOKIES.get ('productid',"NA")
+    print("PID is ", p_id)
+
     template = loader.get_template("prodlist.html")
 
     items = Product.objects.all()
 
 
     data = {
-        "products" : items
+        "products" : items,
+        "browsed_products" : p_id
     }
     res = template.render(data, req)
     return HttpResponse(res)
 
 
-def myProducts(req, aaa):
+def myProducts(req, pid):
+
+
+
     template = loader.get_template("product.html")
-    item = Product.objects.get (id = aaa)
+    item = Product.objects.get (id = pid)
     data = {
 
         "product" : item
 
     }
     res = template.render(data, req)
-    return HttpResponse(res)
+    httpResponse = HttpResponse(res)
 
-    #return HttpResponse ("This is my products page")
+
+    p_ids = req.COOKIES['productid']
+    if p_ids:
+        p_ids= p_ids + "|" +pid
+    else :
+        p_ids = pid
+
+
+    max_age = 365 * 24 * 60 * 60
+    httpResponse.set_cookie("productid" , p_ids, max_age  )
+    return httpResponse
+
+def addItem (request):
+    pid = request.GET ['pid']
+    qty = request.GET['qty']
+
+    cartItems = {}
+
+    if request.session.__contains__("cart"):
+        cartItems = request.session['cart']
+
+    cartItems[pid] = qty
+    request.session['cart'] = cartItems 
+    print(request.session['cart'])
+    return HttpResponse("Added Item to Cart")
+
